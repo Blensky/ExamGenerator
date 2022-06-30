b3936ec7ab59b7d6f4e95ed59b5b5172541ece52
Blensky <berlenskyleger@gmail.com>
2022-06-30 19:58:34 -0400

    Base de donnees pour stocker les questions et les reponses

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 #Application qui permet de generer un examen et son correctum sur commande
+import os.path
+import sqlite3
 
-    
+#La base de donnees
+def database():
+    if os.path.isfile('examBank.db'):
+        dbfile = r'C:\Users\berle\PycharmProjects\ExamGenerator\examBank.db'
+    else :
+        conn = sqlite3.connect('examBank.db')
+        dbfile = r'C:\Users\berle\PycharmProjects\ExamGenerator\examBank.db'
+
+#Creation d'une Fonction qui permet de stocker les questions, leurs types.
+#Commencons par les questions a choix multiples
+'''Puisqu'il existe 3 types de questions, on pourrait d'abord considerer chacun de ces types comme un objet.
+Et ainsi creer pour chacun une fonction qui fera la gestion de son comportement'''
+
+def quest_choix():
+    question = input("Saisir le libelle de votre question: ")
+    #stocker la question.
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -9,5 +26,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    database()
+   # quest_choix()
 
